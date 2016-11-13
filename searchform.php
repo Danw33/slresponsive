9c0ee812bc398a5a7618d6b8dcc8c776e31179d6
Eugene <e.kvasnyi@awodev.ru>
2016-11-14 00:08:52 +0200

    text domain added

--- searchform.php
+++ searchform.php
@@ -1,9 +1,9 @@
 <form role="search" method="get" class="form-inline" action="<?php echo home_url( '/' ); ?>">
     <div class="form-group">
         <input type="search" class="form-control serch-input"
-            placeholder="<?php echo esc_attr_x( 'Search …', 'slresponsive' ) ?>"
+            placeholder="<?php echo __( 'Search …', 'slresponsive' ) ?>"
             value="<?php echo get_search_query() ?>" name="s"
-            title="<?php echo esc_attr_x( 'Search for:', 'slresponsive' ) ?>" />
-		<input type="submit" class="btn btn-primary" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
+            title="Search for" />
+		<input type="submit" class="btn btn-primary" value="<?php echo __( 'Search', 'submit button' ) ?>" />
     </div>
 </form>
